c39fd32e32c79eedc2d6f98083658ce19a484c84
𝓔𝓶𝓶𝓪 (𝓥𝓲𝓸𝓵𝓮𝓽'𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷) <145789380+Elpapiema@users.noreply.github.com>
2025-02-12 14:15:50 -0600

    Actualizar

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('github').addEventListener('click', function() {
+    window.open('https://github.com/Elpapiema/AlyaBot_MD', '_blank');
+});
+
+document.getElementById('descargar').addEventListener('click', function() {
+    window.open('https://github.com/Elpapiema/AlyaBot_MD/archive/refs/heads/main.zip', '_blank');
+});
